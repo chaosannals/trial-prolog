7f615b4f8a3c48ac71208939e7b496448d431f9e
chaosannals <chaosannals@outlook.com>
2022-11-15 10:55:20 +0800

    save

--- d1.pl
+++ d1.pl
@@ -1,4 +1,10 @@
+% 关系定义 %
 myrelation(a, b).
 myrelation(c, d).
 
-myrule(A, B) :- myrelation(B, A).
+% 规则定义，定义带变量参数的关系 %
+myrelation(A, B) :- myrelation(B, A).
+
+myrule(A, B, C) :-
+    myrelation(B, A),
+    \+ myrelation(A, C).
